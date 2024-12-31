3c019bab89f1d23a214b9461985847942a890b0d
hal <72111003+devder@users.noreply.github.com>
2024-12-31 05:38:24 +0100

    create fail on error util

--- listener_service/main.go
+++ listener_service/main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"os"
 
+	"github.com/devder/gopher_ms/listener/util"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	// connect to rabbit-mq
 	rabbitConn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
-	failOnError(err, "Failed to connect to RabbitMQ")
+	util.FailOnError(err, "Failed to connect to RabbitMQ")
 	log.Println("Connected to RabbitMQ")
 	defer rabbitConn.Close()
 
@@ -19,10 +20,4 @@ func main() {
 	// consume
 
 	// watch the queue and consume events
-}
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
 }

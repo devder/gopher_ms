a2ab49d7adafb1d58682e00458c3153aa11ec0ad
hal <72111003+devder@users.noreply.github.com>
2025-01-01 14:31:52 +0100

    create WriteLog go function

---
+++ logger_service/cmd/api/grpc.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+
+	"github.com/devder/gopher_ms/logger/data"
+	"github.com/devder/gopher_ms/logger/logs"
+)
+
+type LogServer struct {
+	logs.UnimplementedLogServiceServer
+	Models data.Models
+}
+
+func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogEntry()
+
+	// write log
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	err := l.Models.LogEntry.Insert(logEntry)
+	if err != nil {
+		res := &logs.LogResponse{Result: "failed"}
+		return res, err
+	}
+
+	res := &logs.LogResponse{Result: "logged"}
+	return res, nil
+}

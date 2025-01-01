cbf5cddb56d20b3a38839bf6e7678d836f467ec0
hal <72111003+devder@users.noreply.github.com>
2025-01-01 15:01:10 +0100

    start grpc server

--- logger_service/cmd/api/grpc.go
+++ logger_service/cmd/api/grpc.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
 
 	"github.com/devder/gopher_ms/logger/data"
 	"github.com/devder/gopher_ms/logger/logs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type LogServer struct {
@@ -29,4 +34,22 @@ func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.L
 
 	res := &logs.LogResponse{Result: "logged"}
 	return res, nil
+}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRpcPort))
+	if err != nil {
+		log.Fatalf("Failed to listen for gRPC: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	logs.RegisterLogServiceServer(grpcServer, &LogServer{Models: app.Models})
+	// consider this self documentation to help the client
+	reflection.Register(grpcServer)
+
+	log.Printf("start gRPC Server at %s", lis.Addr().String())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC: %v", err)
+	}
 }
